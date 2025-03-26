b51b7df56bf2d31f63186568b4b91ceb9c47db74
sunidhi1213 <166869722+sunidhi1213@users.noreply.github.com>
2025-03-26 16:24:23 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -12,3 +12,17 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
+
+def fix_phone_num_ver2(phone_num_to_fix):
+  # given "5125558823". Split the parts, then recombine and return
+  area_code = phone_num_to_fix[0:3] # 512 (first three digits)
+  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
+  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
+  
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part '
+  return fixed_num
+
+
+  
